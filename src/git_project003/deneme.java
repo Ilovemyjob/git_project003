c6446b2e873d8e6dc1582d2535cfb08e74a94204
Emrah Alpaslan <eailovemyjob@gmail.com>
2020-08-11 07:21:59 +0200

    1. from local

---
+++ src/git_project003/deneme.java
@@ -0,0 +1,12 @@
+package git_project003;
+
+public class deneme {public deneme() {
+	// TODO Auto-generated constructor stub
+}public static void main(String[] args) {
+	
+	
+	System.out.println("1. from local");
+}
+	
+	
+}
